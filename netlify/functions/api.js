0868725c9b509a639f9835fd75f8f49de1995529
Devied Rahmadsyah <deviedcodev@gmail.com>
2026-03-29 11:23:20 +0000

    4

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 
+// CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -15,12 +16,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// Route contoh (tanpa prefix /api)
+// Route contoh
 app.get("/hello", (req, res) => {
-  res.json({ message: "Hello dari Netlify Function! ✅" });
+  res.json({
+    message: "Hello dari Netlify Function! ✅",
+    success: true,
+  });
 });
-
-// Tambahkan route lain di sini nanti, contoh:
-// app.get('/users', async (req, res) => { ... });
 
 export const handler = serverless(app);
